59c9ac0ca8b7bc5cd1d6b5432e71e5180193984d
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-11-03 20:53:54 +0900

    -dsgfhgfdbregbnrtehngfhtnv byt

--- src/components/Stats/NowStatus.vue
+++ src/components/Stats/NowStatus.vue
@@ -1,46 +1,41 @@
 <template>
-    <div>
-        <v-card
-            color="#8FA3FF"
-            dark
-          > 
-            
-            <v-card-title class="text-h6 dark">
-                {{status.companyName}} 출근 현황
-            </v-card-title>
-          </v-card>
-          <br/>
-          <v-card
-            color="#8FA3FF"
-            dark
-          > 
-            <v-card-title class="text-h6 dark">
-                {{status.allWorker}}명의 사원중
-            </v-card-title>
+  <div>
+    <v-card color="#8FA3FF" dark>
+      <v-card-title class="text-h6 dark">
+        {{ status.companyName }} 출근 현황
+      </v-card-title>
+    </v-card>
+    <br />
+    <v-card color="#8FA3FF" dark>
+      <v-card-title class="text-h6 dark">
+        {{ status.allWorker }}명의 사원중
+      </v-card-title>
 
-            <v-card-text class="text-h6 dark">{{status.nowWorker}} 명이 출근했어요.</v-card-text>
-          </v-card>
-    </div>
+      <v-card-text class="text-h6 dark"
+        >{{ status.nowWorker }} 명이 출근했어요.</v-card-text
+      >
+    </v-card>
+  </div>
 </template>
-<script>
+<script lang="ts">
 export default {
-    props:{
-        status:{
-            companyName:String,
-            allWorker:Number,
-            nowWorker:Number
-        }
-    }
-}
+  props: {
+    status: {
+      companyName: String,
+      allWorker: Number,
+      nowWorker: Number,
+    },
+  },
+};
 </script>
 <style lang="css">
-    .dark{
-        color:#fff;
-    }
-    .title{
-        font-size: 14px;
-    }
-    .space{
-        margin-top: 15px;
-    }
+.dark {
+  color: #fff;
+}
+.title {
+  font-size: 14px;
+}
+.space {
+  margin-top: 15px;
+}
 </style>
--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        <NowStatus v-bind:status="status"></NowStatus>
-    </div>
+  <div>
+    <NowStatus v-bind:status="status"></NowStatus>
+  </div>
 </template>
 <script lang="ts">
-import NowStatus from '../components/Stats/NowStatus.vue';
+import NowStatus from "../components/Stats/NowStatus.vue";
 export default {
-    name:"Status",
-    components:{NowStatus},
-    data() {
-        return {
-            status:{companyName:"마이다스 IT",allWorker:999,nowWorker:10}
-        }
-    },
-}
+  name: "Status",
+  components: { NowStatus },
+  data() {
+    return {
+      status: { companyName: "마이다스 IT", allWorker: 999, nowWorker: 10 },
+    };
+  },
+};
 </script>
 
-<style>
-    
-</style>
+<style></style>
